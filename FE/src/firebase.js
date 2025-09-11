b0a8d3b749df549a3c30d0affa759baeff106e69
Gnanika710 <127465261+Gnanika710@users.noreply.github.com>
2025-09-11 14:59:34 +0530

    Update firebase.js

--- FE/src/firebase.js
+++ FE/src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBQ1zBepJ4_qSCEhbnHjz0qyYlR8c2G8js",
-  authDomain: "majorproject-2d32b.firebaseapp.com",
-  projectId: "majorproject-2d32b",
-  storageBucket: "majorproject-2d32b.firebasestorage.app",
-  messagingSenderId: "482654287158",
-  appId: "1:482654287158:web:d3c6279950f539ebd06f97",
-  measurementId: "G-FK5MHP9F0T"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
